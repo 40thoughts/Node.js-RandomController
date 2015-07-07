9c6f6904408dea229a50cb33ddea6772190239ed
40thoughts <40thoughts@gmail.com>
2015-07-07 04:03:46 +0200

    Integrated SerialPort, added a thermal printer library

--- routes/index.js
+++ routes/index.js
@@ -5,43 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var appName = req.app.get('appName');
   var page = req.query.page;
+
   res.render('index', { appName: appName, page: page });
-});
-
-/* POST the printer page. */
-router.post('/', function(req, res, next) {
-  var appName = req.app.get('appName');
-  var page = req.query.page;
-
-  var SerialPort = require("serialport").SerialPort;
-  var serialPort = new SerialPort("/dev/ttyUSB0", {
-    baudrate: 9600,
-    rtscts: true
-  });
-
-  serialPort.on("open", function () {
-    serialPort.on('data', function(data) {
-      if(data != " ") {
-        if(data == "$") {
-          console.log('\x1b[41mPlease put some paper in you printer, it\'s empty !\x1b[0m');
-          var message = 'Please put some paper in you printer, it\'s empty !';
-        }
-        else {
-          console.log('\x1b[41mCheck your printer, it seems that there is an error !\x1b[0m');
-          var message = 'Check your printer, it seems that there is an error !';
-        }
-        res.render('index', { appName: appName, message: message, page: page });
-      }
-      else {
-        if (req.body.printText) {
-          serialPort.write(req.body.printText + "\n");
-        }
-        res.render('index', { appName: appName, page: page });
-      }
-    });
-    serialPort.write("\x1b\x40");
-    serialPort.write("\x1b\x76\x00");
-  });
 });
 
 module.exports = router;
--- routes/print.js
+++ routes/print.js
@@ -1,82 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
-function printerReset() {
-}
+var printer = require('../lib/thermalPrinter');
 
 /* GET the printer page. */
 router.get('/', function(req, res, next) {
-  var appName = req.app.get('appName');
+  var text = req.query.text;
 
-  var SerialPort = require("serialport").SerialPort;
-  var serialPort = new SerialPort("/dev/ttyUSB0", {
-    baudrate: 9600,
-    rtscts: true
-  });
-
-  serialPort.on("open", function () {
-    serialPort.on('data', function(data) {
-//    console.log('data received: ' + data);
-      if(data != " ") {
-        if(data == "$") {
-          console.log('\x1b[41mPlease put some paper in you printer, it\'s empty !\x1b[0m');
-          var message = 'Please put some paper in you printer, it\'s empty !';
-        }
-        else {
-          console.log('\x1b[41mCheck your printer, it seems that there is an error !\x1b[0m');
-          var message = 'Check your printer, it seems that there is an error !';
-        }
-        res.render('print', { appName: appName, message: message });
-      }
-      else {
-        if (req.query.line) {
-          serialPort.write(req.query.line + "\n");
-        }
-    res.render('print', { appName: appName });
-      }
-    });
-    serialPort.write("\x1b\x40");
-    serialPort.write("\x1b\x76\x00");
-  /*  serialPort.write("Salut qsdmlqsdmlk\n", function(err, results) {
-      console.log('err ' + err);
-      console.log('results ' + results);
-    });*/
-  });
-});
-
-/* POST the printer page. */
-router.post('/', function(req, res, next) {
-  var appName = req.app.get('appName');
-
-  var SerialPort = require("serialport").SerialPort;
-  var serialPort = new SerialPort("/dev/ttyUSB0", {
-    baudrate: 9600,
-    rtscts: true
-  });
-
-  serialPort.on("open", function () {
-    serialPort.on('data', function(data) {
-      if(data != " ") {
-        if(data == "$") {
-          console.log('\x1b[41mPlease put some paper in you printer, it\'s empty !\x1b[0m');
-          var message = 'Please put some paper in you printer, it\'s empty !';
-        }
-        else {
-          console.log('\x1b[41mCheck your printer, it seems that there is an error !\x1b[0m');
-          var message = 'Check your printer, it seems that there is an error !';
-        }
-        res.render('print', { appName: appName, message: message });
-      }
-      else {
-        if (req.body.printText) {
-          serialPort.write(req.body.printText + "\n");
-        }
-        res.render('print', { appName: appName });
-      }
-    });
-    serialPort.write("\x1b\x40");
-    serialPort.write("\x1b\x76\x00");
-  });
+  printer.printText(text);
+  res.sendStatus(200);
 });
 
 module.exports = router;
